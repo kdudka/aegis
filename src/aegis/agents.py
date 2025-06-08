29d61f0b783f25f7df53b6e9bb0a8f39289e0d09
JimFuller-RedHat <jfuller@redhat.com>
2025-06-08 17:53:47 +0200

    enhance tooling more

--- src/aegis/agents.py
+++ src/aegis/agents.py
@@ -4,17 +4,19 @@ aegis agents
 
 from pydantic_ai import Agent
 from pydantic_ai.common_tools.duckduckgo import duckduckgo_search_tool
+from pydantic_ai.common_tools.tavily import tavily_search_tool
 
 from aegis import (
     default_data_deps,
     default_llm_model,
     llm_model,
+    tavily_api_key,
 )
 from aegis.features.data_models import AegisAnswer
 from aegis.rag import rag_lookup, RAGResponse
 from aegis.tools import date_tool
-from aegis.tools.osidb import osidb_tool
 from aegis.tools.wikipedia import wikipedia_tool
+
 
 base_agent = Agent(
     name="Plain Agent",
@@ -40,7 +42,7 @@ component_feature_agent = Agent(
         "top_p": 0.8,
         "response_format": {"type": "json_object"},
     },
-    tools=[wikipedia_tool],
+    tools=[date_tool, wikipedia_tool],
 )
 
 feature_agent = Agent(
@@ -76,7 +78,12 @@ context_agent = Agent(
     description="An agent that pulls in all the context.",
     model=default_llm_model,
     llm=llm_model,
-    tools=[date_tool, duckduckgo_search_tool(), wikipedia_tool, osidb_tool],
+    tools=[
+        date_tool,
+        tavily_search_tool(tavily_api_key),
+        duckduckgo_search_tool(),
+        wikipedia_tool,
+    ],
     model_settings={
         "temperature": 0.05,
         "top_p": 0.8,
