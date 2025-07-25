9037d8dc16bfc8493725319fb6d0f715fb6e13c7
JimFuller-RedHat <jfuller@redhat.com>
2025-07-25 12:45:44 +0200

    chore:align cvss validation

--- src/aegis/data_models.py
+++ src/aegis/data_models.py
@@ -1,20 +1,14 @@
 # common data models/fields
 
-import re
 from typing import Annotated
 
 import cvss
-from pydantic import StringConstraints, AfterValidator, TypeAdapter
+from pydantic import StringConstraints, TypeAdapter, BeforeValidator
 
 
 def is_cvss3_valid(cvss_str: str) -> bool:
     """return True if cvss_str is a valid CVSS3 vector"""
     try:
-        # FIXME: cvss_str is *sometimes* prefixed with the actual score, which
-        # breaks the validation.  Should we canonicalize the output from Aegis
-        # instead?
-        cvss_str = re.sub(r"^[0-9]+(\.[0-9])+/", "", cvss_str)
-
         cvss.cvss3.CVSS3(cvss_str)
         return True
 
@@ -32,11 +26,10 @@ def validate_with_is_cvss3(v: str) -> str:
 CVSS3Vector = Annotated[
     str,
     StringConstraints(
-        pattern=r"^[0-9]+(\.[0-9])+/",
         strict=True,
         strip_whitespace=True,
     ),
-    AfterValidator(validate_with_is_cvss3),
+    BeforeValidator(validate_with_is_cvss3),
 ]
 
 # cve id field
