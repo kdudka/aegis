kdudka/aegis

commit 3e4c0e58983ea13fdf738ba57894edbb5fa65f19

chore: refactor the code of `run_if_safe()`
... to make it easier to extend.  No change in behavior intended.

Related: https://issues.redhat.com/browse/AEGIS-188

src/aegis_ai/features/__init__.py
@@ -18,6 +18,16 @@ class Feature:
     def __init__(self, agent: Agent):
         self.agent = agent
 
+    async def _timeout_wrap(self, prompt, **kwargs):
+        try:
+            runner = self.agent.run(prompt.to_string(), **kwargs)
+            return await asyncio.wait_for(runner, timeout=llm_prompt_timeout)
+
+        except asyncio.TimeoutError:
+            msg = f"{self.__class__.__name__}: LLM request timed out after {llm_prompt_timeout} seconds"
+            logger.warning(msg)
+            raise RuntimeError(msg)
+
     async def run_if_safe(self, prompt, **kwargs):
         """
         Execute `self.agent.run(...)` only if the provided prompt passes `prompt.is_safe()`.
@@ -29,15 +39,5 @@ class Feature:
                 logger.info(msg)
                 raise RuntimeError(msg)
 
-            try:
-                return await asyncio.wait_for(
-                    self.agent.run(
-                        prompt.to_string(),
-                        **kwargs,
-                    ),
-                    timeout=llm_prompt_timeout,
-                )
-            except asyncio.TimeoutError:
-                msg = f"{self.__class__.__name__}: LLM request timed out after {llm_prompt_timeout} seconds"
-                logger.warning(msg)
-                raise RuntimeError(msg)
+        result = await self._timeout_wrap(prompt, **kwargs)
+        return result

commit 7198de65333341881162071bf3c064c946966d8a

fix: make `AEGIS_LLM_MAX_JOBS` work again
After refactoring the code of `run_if_safe()` one line was incorrectly
indented, which prevented the `AEGIS_LLM_MAX_JOBS` feature from taking
an effect.  This commit fixes it.

Fixes: commit 3e4c0e58983ea13fdf738ba57894edbb5fa65f19
Related: https://issues.redhat.com/browse/AEGIS-188

src/aegis_ai/features/__init__.py
@@ -42,7 +42,7 @@ class Feature:
                 logger.info(msg)
                 raise RuntimeError(msg)
 
-        result = await self._timeout_wrap(prompt, **kwargs)
+            result = await self._timeout_wrap(prompt, **kwargs)
 
         # check how many input tokens were processed by the LLM
         input_tokens = result._state.usage.input_tokens