21a5e8adfb8ba2feb3318bcf704f22a6b843e226
Kamil Dudka <kdudka@redhat.com>
2025-10-15 09:32:37 +0200

    fix: trigger build of the FAISS index
    ... before starting the web service.  This ensures there will be no
    excessive delays in Aegis responses after rollout in OpenShift (because
    the routing to the new pod will not be updated until the FAISS index is
    built).
    
    Fixes: https://github.com/RedHatProductSecurity/aegis-ai/issues/262
    Related: https://issues.redhat.com/browse/AEGIS-205

--- scripts/run_web_service.sh
+++ scripts/run_web_service.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
 
+# trigger build of the FAISS index
+uv run aegis suggest-cwe CVE-2025-23395
+
+# run the web service
 uv run uvicorn aegis_ai_web.src.main:app --port 9000 --loop uvloop --http httptools --host 0.0.0.0
