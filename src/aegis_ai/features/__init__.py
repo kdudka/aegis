a10e66714e792721284753295bbb409eff092fc1
Kamil Dudka <kdudka@redhat.com>
2025-11-24 14:08:43 +0100

    chore: increase default `AEGIS_LLM_INPUT_TOKENS_WARN_THR`
    ... such that the warning is not logged under normal circumstances.

--- src/aegis_ai/features/__init__.py
+++ src/aegis_ai/features/__init__.py
@@ -16,7 +16,7 @@ llm_max_jobs = int(os.getenv("AEGIS_LLM_MAX_JOBS", "4"))
 llm_sem = asyncio.Semaphore(llm_max_jobs)
 
 # The threshold for LLM input tokens to log a warning
-llm_input_tokens_warn_thr = int(os.getenv("AEGIS_LLM_INPUT_TOKENS_WARN_THR", 16384))
+llm_input_tokens_warn_thr = int(os.getenv("AEGIS_LLM_INPUT_TOKENS_WARN_THR", 65536))
 
 
 # initial delay in seconds after getting HTTP 503 status code from LLM (doubled on each attempt)
