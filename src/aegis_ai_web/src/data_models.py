kdudka/aegis

commit b1b75adc6ac074f7129c6026fd47da3b60854dfa

fix: an interim fix for OSIM-submitted feedback
OSIM currently submits the CVE ID in the `cveid` field to the Aegis
API endpoint while Aegis expected it in the `cve_id` field.  This is
an interim fix to make Aegis take the information from any of those
fields (while preferring `cve_id`) until OSIM is fixed to use the
`cve_id` field for consistency across all the API endpoints.

Related: https://issues.redhat.com/browse/AEGIS-294

src/aegis_ai_web/src/data_models.py
@@ -23,6 +23,9 @@ class Feedback(BaseModel):
 
     feature: str = Field(..., max_length=100)
     cve_id: Optional[CVEID] = Field("", max_length=50)
+    cveid: Optional[CVEID] = Field(
+        "", max_length=50
+    )  # FIXME: an interim compatibility fix for OSIM
     email: Optional[str] = Field("", max_length=100)
     request_time: Optional[str] = Field("", max_length=50)
     actual: Optional[str] = Field("", max_length=5000)  # Increased for longer values

src/aegis_ai_web/src/main.py
@@ -436,7 +436,9 @@ async def save_feedback(feedback: Feedback):
         accept_str = str(feedback.accept).lower()
         row_data = {
             "feature": feedback.feature,
-            "cve_id": feedback.cve_id or "",
+            "cve_id": feedback.cve_id
+            or feedback.cveid
+            or "",  # FIXME: an interim compatibility fix for OSIM
             "email": feedback.email or "",
             "actual": feedback.actual or "",
             "expected": feedback.expected or "",

commit 6696a744074791b685e333500769d2ace65038c7

fix: correct the field name in the last commit
OSIM submits a field named `cveId` rather than `cveid`, so the interim
fix was not effective.  This commit fixes it.

Fixes: commit b1b75adc6ac074f7129c6026fd47da3b60854dfa
Related: https://issues.redhat.com/browse/AEGIS-294

src/aegis_ai_web/src/data_models.py
@@ -23,7 +23,7 @@ class Feedback(BaseModel):
 
     feature: str = Field(..., max_length=100)
     cve_id: Optional[CVEID] = Field("", max_length=50)
-    cveid: Optional[CVEID] = Field(
+    cveId: Optional[CVEID] = Field(
         "", max_length=50
     )  # FIXME: an interim compatibility fix for OSIM
     email: Optional[str] = Field("", max_length=100)

src/aegis_ai_web/src/main.py
@@ -437,7 +437,7 @@ async def save_feedback(feedback: Feedback):
         row_data = {
             "feature": feedback.feature,
             "cve_id": feedback.cve_id
-            or feedback.cveid
+            or feedback.cveId
             or "",  # FIXME: an interim compatibility fix for OSIM
             "email": feedback.email or "",
             "actual": feedback.actual or "",