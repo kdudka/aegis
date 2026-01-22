0e59a71587bfc129496be337b5d70af5285f7430
Kamil Dudka <kdudka@redhat.com>
2026-01-22 18:33:31 +0100

    chore: rework the `process_cve_feedback_form.py` script
    ... to handle feedback data in the new format
    
    Related: https://issues.redhat.com/browse/AEGIS-284

--- scripts/process_cve_feedback_form.py
+++ scripts/process_cve_feedback_form.py
@@ -5,11 +5,27 @@ import csv
 import json
 import re
 import sys
-from typing import no_type_check
 
 from aegis_ai import config_logging
 from aegis_ai.toolsets.tools.cwe import cwe_manager
 from evals.utils.osidb_cache import osidb_cache_retrieve
+
+FEEDBACK_CSV_HEADER = [
+    "datetime",
+    "feature",
+    "cve_id",
+    "email",
+    "actual",
+    "expected",
+    "request_time",
+    "accept",
+    "rejection_comment",
+    "version",
+]
+
+# load CWE-699 view so that we can annotate CWEs not present in the view
+asyncio.run(cwe_manager.initialize())
+cwe_defs = cwe_manager._definitions
 
 
 def _cve_sort_key(cve_id):
@@ -35,119 +51,95 @@ def osidb_cache_cve(cve):
         print(f"{' ' * 4}# NOTE: failed to cache {cve} from OSIDB: {e}")
 
 
-@no_type_check
-def process_cwe_feedback(rows):
-    """processor of suggest-cwe feedback"""
-    # Try to load CWE-699 view to flag CWEs not present there
-    asyncio.run(cwe_manager.initialize())
-    cwe_defs = cwe_manager._definitions
+def describe_feature(feature) -> tuple[str, str]:
+    match feature:
+        case "suggest-cvss":
+            evaluator = "Impact"
+            field = "expected_cvss3_vector"
 
-    # column headers vary in time
-    header_cve = {"Column 1", "CVE-ID"}
-    header_exp_cwe = {"Column 3", "Expected CWE value"}
+        case "suggest-cwe":
+            evaluator = "Cwe"
+            field = "cwe_list"
 
-    # there can be multiple rows for a single CVE, for which we merge the CWE lists
-    data = []
-    for row in rows:
-        feature = row[8]
-        if feature and feature != "suggest-cwe":
-            # skip feedback for other features
-            continue
+        case "suggest-description":
+            evaluator = "Description"
+            field = "expected_description"
 
-        cve = row[1]
-        exp_cwe = row[3]
-        if cve in header_cve and exp_cwe in header_exp_cwe:
-            # skip table header
-            continue
+        case "suggest-impact":
+            evaluator = "Impact"
+            field = "expected_impact"
 
-        if not exp_cwe.strip():
-            # skip a row with no expected CWE
-            continue
+        case "suggest-mitigation":
+            evaluator = "Statement"
+            field = "expected_mitigation"
 
-        # remove parentheses in case we get "(CWE-119|CWE-122)"
-        exp_cwe = exp_cwe.strip("()")
+        case "suggest-statement":
+            evaluator = "Statement"
+            field = "expected_statement"
 
-        # create a well formatted list out of the full-text field
-        cwe_list = [item.strip() for item in re.split(r" *(?:[,/|]|or) *", exp_cwe)]
+        case "suggest-title":
+            evaluator = "Description"
+            field = "expected_title"
 
-        # replace NNN or NNNN by CWE-NNN or CWE-NNNN, respectively
-        cwe_list = [re.sub(r"^([0-9]{3,4})$", r"CWE-\1", cwe) for cwe in cwe_list]
+        case _:
+            assert False, f"unknown feature: {feature}"
 
-        # the rows are sorted by CVE -> check for subsequent rows with identical CVE
-        if data and data[-1][0] == cve:
-            # merge the CWE list with the last item
-            data[-1][1].extend(cwe_list)
-        else:
-            # create a new item (for a previously unseen CVE)
-            data.append((cve, cwe_list))
-
-    for cve, cwe_list in data:
-        # Optionally warn about CWEs not in the CWE-699 view (MITRE)
-        for cwe in cwe_list:
-            cwe_data = cwe_defs.get(cwe)
-            if not cwe_data or cwe_data.get("disallowed", True):
-                print(f"{' ' * 4}# FIXME: {cwe} is not included in the CWE-699 view!")
-
-        osidb_cache_cve(cve)
-
-        # print single instantiation of SuggestCweCase
-        cwe_list = ", ".join(f'"{cwe}"' for cwe in cwe_list)
-        print(f"{' ' * 4}SuggestCweCase(")
-        print(f'{" " * 8}cve_id="{cve}",')
-        print(f"{' ' * 8}cwe_list=[{cwe_list}],")
-        print(f"{' ' * 4}),")
-
-
-def is_title(value):
-    """guess whether the provided expected value is a title"""
-    return not value.endswith(".") and len(value) < 128
+    return (evaluator, field)
 
 
 def process_generic_feedback(rows, current_evaluator):
     for row in rows:
-        value = row[3]
-        feature = row[8]
-        match feature:
-            case "suggest-cvss":
-                evaluator = "Impact"
-                field = "cvss3_vector"
-
-            case "suggest-description":
-                evaluator = "Description"
-                field = "title" if is_title(value) else "description"
-
-            case "suggest-impact":
-                evaluator = "Impact"
-                field = "impact"
-
-            case "suggest-mitigation":
-                evaluator = "Statement"
-                field = "mitigation"
-
-            case "suggest-statement":
-                evaluator = "Statement"
-                field = "statement"
-
-            case "suggest-title":
-                evaluator = "Description"
-                field = "title"
-
-            case _:
-                # skip feedback for other features
-                continue
-
+        feature = row[1]
+        evaluator, field = describe_feature(feature)
         if evaluator != current_evaluator:
             # skip feedback for other evaluators
             continue
 
-        # get CVE ID
-        cve = row[1]
+        # get CVE ID and fetch the corresponding fixture from OSIDB
+        cve = row[2]
         osidb_cache_cve(cve)
+
+        # value suggested by Aegis
+        suggested = row[5]
+
+        # check whether we got positie or negative feedback
+        accept = row[7]
+        match accept:
+            case "true":
+                value = suggested
+
+            case "false":
+                suggested_quoted = json.dumps(suggested)
+                rejection_comment = json.dumps(row[8])
+                value = f"FIXME: suggested={suggested_quoted}, rejection_comment={rejection_comment}"
+
+            case _:
+                assert False, f"unknown 'accept' value: {accept}"
+
+        if field == "cwe_list":
+            # FIXME: uncomment and adapt for the new feedback format
+            # replace NNN or NNNN by CWE-NNN or CWE-NNNN, respectively
+            # cwe_list = [re.sub(r"^([0-9]{3,4})$", r"CWE-\1", cwe) for cwe in cwe_list]
+
+            # annotate CWEs not present in the CWE-699 view
+            for m in re.finditer(r"CWE-[0-9]{1,4}", value):
+                cwe_id = m.group()
+                assert isinstance(cwe_defs, dict)
+                cwe_data = cwe_defs.get(cwe_id)
+                if cwe_data and not cwe_data.get("disallowed", True):
+                    continue
+
+                print(
+                    f"{' ' * 4}# FIXME: {cwe_id} is not included in the CWE-699 view!"
+                )
+
+            # although we have a single value (in 99% cases), the evaluator takes a list
+            value = [value]
 
         # get expected output
         print(f"{' ' * 4}Suggest{evaluator}Case(")
         print(f"{' ' * 8}cve_id={json.dumps(cve)},")
-        print(f"{' ' * 8}expected_{field}={json.dumps(value)},")
+        print(f"{' ' * 8}{field}={json.dumps(value)},")
         print(f"{' ' * 4}),")
 
 
@@ -157,12 +149,15 @@ def process_feedback(file_path):
         csv_reader = csv.reader(input_file)
         rows = list(csv_reader)
 
+    # check CSV header end remove the row
+    assert rows[0] == FEEDBACK_CSV_HEADER
+    rows.pop(0)
+
     # sort the list by CVE ID
-    rows.sort(key=lambda r: _cve_sort_key(r[1]))
+    rows.sort(key=lambda r: _cve_sort_key(r[2]))
 
     # run specific processors
-    process_cwe_feedback(rows)
-    for evaluator in ("Description", "Impact", "Statement"):
+    for evaluator in ("Cwe", "Description", "Impact", "Statement"):
         process_generic_feedback(rows, evaluator)
 
 
