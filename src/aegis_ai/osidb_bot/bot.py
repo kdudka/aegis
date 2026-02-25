401ec5d31508f8e69a2323e578b0905937fb6f6d
Kamil Dudka <kdudka@redhat.com>
2026-02-25 15:36:04 +0100

    osidb-bot: optimize the search in `FlawFinder`
    ... by using a large page size to minimize round trips when only
    fetching CVE IDs.
    
    Related: https://issues.redhat.com/browse/AEGIS-352

--- src/aegis_ai/osidb_bot/bot.py
+++ src/aegis_ai/osidb_bot/bot.py
@@ -92,6 +92,8 @@ class FlawFinder:
                 cast(dict[str, str], c)["state"]
                 for c in ELIGIBLE_FLAWS["classification"]
             ],
+            # use a large page size to minimize round trips when only fetching IDs
+            "limit": 1000,
         }
 
         # emptiness predicates for indexed fields
