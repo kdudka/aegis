c8169521cc2aceb5c771789905c8ad6c0cdda04c
Kamil Dudka <kdudka@redhat.com>
2026-03-23 12:52:24 +0100

    evals: annotate an occasionally failing evaluation case
    ```
    FAILED evals/features/cve/test_suggest_cwe.py::test_eval_suggest_cwe - AssertionError: Unsatisfied assertion(s):
    suggest-cwe-for-CVE-2022-50421: CWEExplanationRootCause: False, reason: The explanation clearly describes a plausible technical weakness (use-after-free, double free). However, the listed CWE-911 (Improperly Implemented Security Check for Standard) does not reasonably relate to the described memory corruption flaw.
    ```

--- evals/features/cve/test_suggest_cwe.py
+++ evals/features/cve/test_suggest_cwe.py
@@ -110,7 +110,12 @@ cases = [
     SuggestCweCase(
         cve_id="CVE-2022-50421",
         cwe_list=["CWE-1341"],
-        metadata={"known_to_fail_evaluators": ["SuggestCweEvaluator"]},
+        metadata={
+            "known_to_fail_evaluators": [
+                "CWEExplanationRootCause",
+                "SuggestCweEvaluator",
+            ]
+        },
     ),
     SuggestCweCase(
         cve_id="CVE-2022-50439",
